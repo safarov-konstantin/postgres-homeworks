00a03627a26775c90912e57d90546531b8000a91
Konstantin Safarov <safarov-konstantin@yandex.ru>
2023-11-14 20:26:50 +0300

    изменено:      homework-1/create_tables.sql 	изменено:      homework-1/main.py

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,25 @@
--- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(100),
+	last_name varchar(100),
+	title varchar(150),
+	birth_date date,
+	notes text
+);
+
+CREATE TABLE customers
+(
+	customer_id varchar(10) PRIMARY KEY,
+	company_name varchar(150),
+	contact_name varchar(150)
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(10) REFERENCES customers(customer_id) NOT NULL,
+	employee_id int REFERENCES employees(employee_id) NOT NULL,
+	order_date date,
+	ship_city varchar(150)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,66 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2, os, csv
+from pathlib import Path
+
+
+SETTING_CONNECT_DATABASE = {
+    "host": "localhost",
+    "database": "north",
+    "user": "postgres",
+    "password": "sc1or6"
+}
+
+
+def get_data_from_csv(file_name='customers_data.csv'):
+    path = os.path.join(Path(__file__).parent.parent, f"homework-1/north_data/{file_name}")
+    data = []
+    with open(path, encoding='utf8') as file:
+        reader = csv.DictReader(file)
+        data = [row for row in reader]
+    return data
+
+
+def clear_table(name_table):
+    with psycopg2.connect(**SETTING_CONNECT_DATABASE) as conn:
+        with conn.cursor() as cur:
+            request_text = f'DELETE FROM {name_table}'
+            cur.execute(request_text)
+        conn.commit()
+
+
+def fill_table_database(name_table, data_csv):
+
+    # Проверка на постой список
+    if len(data_csv) == 0:
+        return None
+
+    with psycopg2.connect(**SETTING_CONNECT_DATABASE) as conn:
+        with conn.cursor() as cur:
+            for row in data_csv:
+                col_values = ['%s' for i in range(len(row))]
+                col_values = ", ".join(col_values)
+                request_text = f'INSERT INTO {name_table} VALUES ({col_values})'
+                value_cur = tuple(row.values())
+                cur.execute(request_text, value_cur)
+        conn.commit()
+
+
+def main():
+
+    # Получение данных их фалов csv
+    customers = get_data_from_csv('customers_data.csv')
+    employees = get_data_from_csv('employees_data.csv')
+    orders = get_data_from_csv('orders_data.csv')
+
+    # Очистить таблицы
+    clear_table('orders')
+    clear_table('employees')
+    clear_table('customers')
+
+    # Заполнение таблиц
+    fill_table_database('customers', customers)
+    fill_table_database('employees', employees)
+    fill_table_database('orders', orders)
+
+
+if __name__ == '__main__':
+    main()
